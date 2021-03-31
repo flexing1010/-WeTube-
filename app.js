1ca3a6393d9cf7fd99a5c328fe250fca6f77ae1b
Jeongjin Kim <flexing1010@JayKim-PC.localdomain>
2021-03-31 17:03:30 +0900

    pug init

--- app.js
+++ app.js
@@ -2,24 +2,22 @@ import express from "express";
 import logger from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 import bodyParser from "body-parser";
-
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from korea");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(logger("dev"));
 
-app.get("/", handleHome);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
 export default app;
