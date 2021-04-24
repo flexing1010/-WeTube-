427ab5bb68a81d77c8ff66497db3918ecda7f670
Jeongjin Kim <kjzon@hotmail.com>
2021-04-24 17:21:32 +0900

    attempt to fix scss bug2

--- src/passport.js
+++ src/passport.js
@@ -1,10 +1,6 @@
 import passport from "passport";
 import GithubStrategy from "passport-github";
-import FacebookStrategy from "passport-facebook";
-import {
-  facebookLoginCallback,
-  githubLoginCallback,
-} from "./controllers/userController";
+import { githubLoginCallback } from "./controllers/userController";
 import User from "./models/User";
 import routes from "./routes";
 
@@ -20,17 +16,6 @@ passport.use(
         : `http://localhost:4000${routes.githubCallback}`,
     },
     githubLoginCallback
-  )
-);
-
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: process.env.FB_ID,
-      clientSecret: process.env.FB_SECRET,
-      callbackURL: `http://localhost:4000${routes.facebookCallback}`,
-    },
-    facebookLoginCallback
   )
 );
 
